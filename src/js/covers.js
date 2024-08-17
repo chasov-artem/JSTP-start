a6694993127b19610b50692e7e84c82be75762d4
chasov-artem <chasov90@gmail.com>
2024-08-17 19:09:16 +0300

    122

---
+++ src/js/covers.js
@@ -0,0 +1,24 @@
+// const coversSection = document.getElementById('covers');
+// const marquees = document.querySelectorAll('.covers-marquee__inner');
+
+// const observer = new IntersectionObserver(
+//   (entries, observer) => {
+//     entries.forEach(entry => {
+//       if (entry.isIntersecting) {
+//         marquees.forEach(marquee => {
+//           marquee.style.animationPlayState = 'running';
+//         });
+//       } else {
+//         marquees.forEach(marquee => {
+//           marquee.style.animationPlayState = 'paused';
+//         });
+//       }
+//     });
+//   },
+//   {
+//     root: null,
+//     threshold: 0.1,
+//   }
+// );
+
+// observer.observe(coversSection);
